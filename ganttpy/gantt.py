8b58f6f5addf1ebd5bda5c625977f4211d25e116
Alan Knee <alanknee@Alans-2020-MacBook-Pro.local>
2025-08-16 23:55:06 -0700

    Create example notebook

--- ganttpy/gantt.py
+++ ganttpy/gantt.py
@@ -11,11 +11,27 @@ DEFAULT_COLOR = 'tab:blue'
 class Task:
     """Class representing a single task with some name, start date, 
     end date (or duration in days), and a color to be used when plotting.
-
-    Dates must be in a format readable by pandas.to_datetime(), e.g.,
-    'Sep 14, 2015' or '2015-09-14'.
     """
-    def __init__(self, name, start, end=None, duration=None, color=None):
+    def __init__(
+        self, 
+        name,
+        start,
+        end=None, 
+        duration=None, 
+        color=DEFAULT_COLOR
+    ):
+        """Initialize a Task.
+
+        Dates must be in a format readable by pandas.to_datetime(), e.g.,
+        'Sep 14, 2015' or '2015-09-14'.
+        
+        Args:
+            name (str): Task name.
+            start (str): Task start date.
+            end (str, optional): Task end date.
+            duration (float, optional): Task duration in days.
+            color (str, optional): Task color.  
+        """
         self.name = name
 
         self.start = pd.to_datetime(start)
@@ -28,50 +44,97 @@ class Task:
         else:
             raise ValueError('Must specify either task end date or duration (in days)')
 
-        if color:
-            self.color = color
-        else:
-            self.color = DEFAULT_COLOR
+        self.color = color or DEFAULT_COLOR
+
+    def __repr__(self):
+        cls = self.__class__.__name__
+        return (
+            f'{cls}(name={self.name}, start={self.start}, '
+            f'end={self.end}, duration={self.duration}, color={self.color})'
+        )
 
 
 def gantt(
-    tasks, 
+    tasks,
+    order,
     begin_date=None, 
-    figsize=(10, 5), 
+    figsize=(10, 3),
+    figtitle=None, 
+    date_freq='7D',
+    date_format='%Y-%m-%d',
     fname=None, 
     return_fig=False
 ):
     """Top-level function for making a Gantt chart. The input must be a list of Task
     objects, each containing a name, start and end dates, and optionally a colour
-    for plotting. Entries are arranged from top to bottom in the order in
+    for plotting. By default, entries are arranged from bottom to top in the order in
     which they appear in the input list.
+
+    Args:
+        tasks (list[Task]): A list of Task instances defining individual tasks.
+        begin_date (str, optional): Beginning date of the time axis.
+        order (str, optional): Task order, either 'listed' (default) for list order 
+            or 'time' to order by start time.
+        figsize (tuple[float], optional): Figure width and height.
+        figtitle (str, optional): Figure title.
+        date_freq (str, optional): Date tick frequency.
+        date_format(str, optional): Date format.
+        fname (str, optional): File path to save figure.
+        return_fig (bool, optional): Return figure and axis objects. 
     """
     # Check that the input is a list of Task objects
+    if not tasks:
+        raise ValueError('Task list is empty')
     for task in tasks:
         if not isinstance(task, Task):
-            raise ValueError('Entries are not all Task objects')
+            raise ValueError(f'Entries are not all Task objects')
+
+    # Task order
+    if order == 'listed':
+        pass
+    elif order == 'time':
+        tasks = sorted(tasks, key=lambda task: task.start.timestamp())
+    else:
+        raise ValueError("Order must be either 'listed' or 'time'")
 
     # If begin_date is given, use it as the beginning of the time axis, 
     # else use the earliest task start date
     if begin_date is None:
-        start_timestamps = [task.start.timestamp() for task in tasks]
-        begin_date = pd.to_datetime(min(start_timestamps), unit='s')
+        begin_date = min(task.start for task in tasks)
     else:
         begin_date = pd.to_datetime(begin_date)
+    end_date = max(task.end for task in tasks)
 
     # Set up figure and add task entries one by one
     fig, ax = plt.subplots(figsize=figsize)
-    for task in tasks:
+    task_idxs = []
+    task_labels = []
+    for idx, task in enumerate(tasks):
         ax.barh(
-            y=task.name,
+            y=idx,
             left=(task.start - begin_date).days,
             width=task.duration.days,
             color=task.color
         )
+        task_idxs.append(idx)
+        task_labels.append(task.name)
+
+    # Task labels
+    ax.set_yticks(task_idxs)
+    ax.set_yticklabels(task_labels)
+    ax.set_title(figtitle)
+
+    # Date labels
+    ax.set_xlim(0, (end_date - begin_date).days)
+    time_ticks = pd.date_range(start=begin_date, end=end_date, freq=date_freq)
+    ax.set_xticks([(tt - begin_date).days for tt in time_ticks])
+    time_labels = [tt.strftime(date_format) for tt in time_ticks]
+    ax.set_xticklabels(time_labels, rotation=45)
+
+    ax.grid(axis='x')
         
     if fname:
         fig.savefig(fname, dpi=400, bbox_inches='tight')
-
     if return_fig:
         return fig, ax
     
